9988821d3df6d2ed4c3a61c6bb38eeeeb75319a2
matthieuEv <evainmatthieu44@gmail.com>
2026-01-12 09:01:51 +0100

    fix sql query

--- database/insertion.sql
+++ database/insertion.sql
@@ -1,3 +1,7 @@
-insert into users (username, password_hash) values ('user', '$2y$10$bojNdcPNbpU8Cv6v9FXcxumADxk9KDLw5ijQT3exaekkRgQB6V7TW'); -- verybigpassword1
+TRUNCATE TABLE tokens;
+TRUNCATE TABLE posts;
+TRUNCATE TABLE users;
 
-insert into posts (id, user_id, title, body, image_url, created_at) values (1, 1, 'First Post', 'This is the content of the first post.', NULL, '2024-01-01 10:00:00');
+INSERT INTO users (username, password_hash) VALUES ('user', '$2y$10$bojNdcPNbpU8Cv6v9FXcxumADxk9KDLw5ijQT3exaekkRgQB6V7TW'); -- verybigpassword1
+
+INSERT INTO posts (id, user_id, title, body, image_url, created_at) VALUES (1, 1, 'First Post', 'This is the content of the first post.', NULL, '2024-01-01 10:00:00');
