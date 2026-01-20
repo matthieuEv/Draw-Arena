cf8011fb2dab5069c5d1440c5c1da43152f0af91
Julien Guitter <julien.guitter@outlook.com>
2026-01-20 18:10:18 +0100

    fix remove index.php

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,5 @@
 // Cette valeur sera remplacée automatiquement lors du déploiement CI/CD
 // Format: window.API_BASE = "BACKEND_API_URL_PLACEHOLDER"
 window.API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? "http://localhost:8000/index.php/api"
+  ? "http://localhost:8000/api"
   : "BACKEND_API_URL_PLACEHOLDER";
