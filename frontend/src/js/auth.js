c24dab93a21748edad4a2bee6441d2a5eaf410cd
Julien Guitter <julien.guitter@outlook.com>
2026-01-22 17:46:45 +0100

    fix temp fix for user image

--- frontend/src/js/auth.js
+++ frontend/src/js/auth.js
@@ -112,7 +112,7 @@ async function login(event) {
       login: payload.login,
       role: res.role,
       club: res.club,
-      photoProfilUrl: payload.photoProfilUrl || null,
+      photoProfilUrl: payload.photo_profil_url || null,
     });
 
     saveSession(res.token, userJSON);
